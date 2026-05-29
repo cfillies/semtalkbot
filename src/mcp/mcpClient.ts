053328d324f319040e231b3a763a09e24b62e8b0
Christian Fillies <cfillies@semtalk.com>
2026-05-29 17:26:57 +0200

    xxx

--- src/mcp/mcpClient.ts
+++ src/mcp/mcpClient.ts
@@ -8,8 +8,8 @@ export async function initMcpClient() {
   if (client) return client;
 
   // const url = process.env.MCP_URL!;
-  const url = "https://semaiservice26.azurewebsites.net/runtime/webhooks/mcp?code=m6Uw9BvzgbI0if-2xYp3AGIcOGJuLuRtz9_S3FlljXH3AzFusFhMIQ==";
-  //  const url = "http://localhost:707/runtime/webhooks/mcp?code=m6Uw9BvzgbI0if-2xYp3AGIcOGJuLuRtz9_S3FlljXH3AzFusFhMIQ==";
+  const url = "https://semaiservice26.azurewebsites.net/runtime/webhooks/mcp?code=_m6Uw9BvzgbI0if-2xYp3AGIcOGJuLuRtz9_S3FlljXH3AzFusFhMIQ==";
+  //  const url = "http://localhost:707/runtime/webhooks/mcp?code=_m6Uw9BvzgbI0if-2xYp3AGIcOGJuLuRtz9_S3FlljXH3AzFusFhMIQ==";
 
   const transport = new StreamableHTTPClientTransport(
     new URL(url)
